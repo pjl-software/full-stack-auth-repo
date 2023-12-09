d2a10587f0e0870e54d838a57ad88c7ae5dd2381
pjl <p.linnehan@gmail.com>
2023-12-08 20:26:24 -0500

    pull in latest code to get the application running with https/ssl

--- spring-back-end/pjl-application/pjl-application-core/src/main/java/com/pjlsoftware/controller/ApplicationCoreController.java
+++ spring-back-end/pjl-application/pjl-application-core/src/main/java/com/pjlsoftware/controller/ApplicationCoreController.java
@@ -2,14 +2,14 @@ package com.pjlsoftware.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class ApplicationCoreController {
-    @GetMapping("health")
+    @RequestMapping(method = RequestMethod.GET, path = "/health")
     public ResponseEntity<String> healthCheck() {
         return new ResponseEntity<>("{\"status\":\"UP\"}", HttpStatus.OK);
     }
