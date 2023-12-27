81d5c4447706811ef67bb668eaf8aabc3968bd0c
pjl <p.linnehan@gmail.com>
2023-12-26 21:25:46 -0500

    add a new service and module placeholder for the health check

--- angular-front-end/src/app/app.module.ts
+++ angular-front-end/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterOutlet } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ApplicationCoreServiceModule } from './pjl-application/pjl-application-core/services/application-core.service.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterOutlet],
+  imports: [BrowserModule, RouterOutlet, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
