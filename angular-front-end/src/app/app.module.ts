f8589c429783d5a33da1917ae4babb81126d5b41
pjl <p.linnehan@gmail.com>
2023-12-27 06:59:36 -0500

    remove unused

--- angular-front-end/src/app/app.module.ts
+++ angular-front-end/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { AppComponent } from './app.component';
 import { RouterOutlet } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { ApplicationCoreServiceModule } from './pjl-application/pjl-application-core/services/application-core.service.module';
 
 @NgModule({
   declarations: [AppComponent],
