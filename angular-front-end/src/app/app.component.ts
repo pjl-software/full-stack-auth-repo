3c63b759722eecb051665787de960fb5a1af151a
pjl <p.linnehan@gmail.com>
2023-12-26 20:31:37 -0500

    refactor app into alignment with other app

--- angular-front-end/src/app/app.component.ts
+++ angular-front-end/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { environment } from '../environment-configs/environment.local';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-front-end';
+  environment = environment.environment;
 }
