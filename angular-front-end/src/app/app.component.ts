418d787a9c108ad1a99d2bf780ccd3d863740e87
pjl <p.linnehan@gmail.com>
2023-12-26 21:24:48 -0500

    refactor component to use async/observable/service health check

--- angular-front-end/src/app/app.component.ts
+++ angular-front-end/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { environment } from '../environment-configs/environment.local';
+import { Observable, of } from 'rxjs';
+import { ApplicationCoreSerivce } from './pjl-application/pjl-application-core/services/application-core.service';
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-front-end';
   environment = environment.environment;
+  backEndHealthStatus$: Observable<string>;
+
+  constructor(private applicationCoreSerivce: ApplicationCoreSerivce) {
+    this.backEndHealthStatus$ = of('not yet set');
+  }
+
+  ngOnInit(): void {
+    this.backEndHealthStatus$ = this.applicationCoreSerivce.getBackEndHealth();
+  }
 }
