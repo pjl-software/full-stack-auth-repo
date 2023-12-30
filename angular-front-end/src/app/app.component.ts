c27219fa2ad19e340c583182a17482432471cc6d
pjl <p.linnehan@gmail.com>
2023-12-30 15:06:16 -0500

    initial commit adding polling with rxjs

--- angular-front-end/src/app/app.component.ts
+++ angular-front-end/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { environment } from '../environment-configs/environment.local';
-import { Observable, of } from 'rxjs';
+import { Observable, interval, of, switchMap } from 'rxjs';
 import { ApplicationCoreSerivce } from './pjl-application/pjl-application-core/services/application-core.service';
 
 @Component({
@@ -19,6 +19,8 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.backEndHealthStatus$ = this.applicationCoreSerivce.getBackEndHealth();
+    this.backEndHealthStatus$ = interval(10000).pipe(
+      switchMap(() => this.applicationCoreSerivce.getBackEndHealth())
+    );
   }
 }
