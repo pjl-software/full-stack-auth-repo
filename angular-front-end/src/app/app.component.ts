ded2d7892cd676c358916f3cd5d67fe700eeee60
pjl <p.linnehan@gmail.com>
2024-01-01 15:10:35 -0500

    use a timer so that we pull immediately and then wait

--- angular-front-end/src/app/app.component.ts
+++ angular-front-end/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { environment } from '../environment-configs/environment.local';
-import { Observable, interval, of, switchMap } from 'rxjs';
+import { Observable, interval, of, startWith, switchMap, timer } from 'rxjs';
 import { ApplicationCoreSerivce } from './pjl-application/pjl-application-core/services/application-core.service';
 
 @Component({
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     const autoRefreshRateInMilliSeconds: number = 5000; // 5 seconds
-    this.backEndHealthStatus$ = interval(autoRefreshRateInMilliSeconds).pipe(
+    this.backEndHealthStatus$ = timer(0, autoRefreshRateInMilliSeconds).pipe(
       switchMap(() => this.applicationCoreSerivce.getBackEndHealth())
     );
   }
