b5e0b71d93e61644a732e6f38bd961f80a03ae45
pjl <p.linnehan@gmail.com>
2024-01-12 21:50:31 -0500

    checkpoint commit of working code

---
+++ spring-back-end/pjl-application-database/pjl-application-database-resources/src/main/java/com/pjlsoftware/configs/DatabaseConfiguration.java
@@ -0,0 +1,42 @@
+package com.pjlsoftware.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = {
+        "com.pjlsoftware.repository"
+})
+@EnableTransactionManagement
+@PropertySource("classpath:database.${profile:local}.properties")
+public class DatabaseConfiguration {
+    @Value("${jdbc.driver}")
+    private String driverClassName;
+
+    @Value("${jdbc.url}")
+    private String url;
+
+    @Value("${jdbc.username}")
+    private String username;
+
+    @Value("${jdbc.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+}
