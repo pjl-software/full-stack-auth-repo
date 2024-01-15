857626b49c3d7ef92192c76686c5ca05556f4b77
pjl <p.linnehan@gmail.com>
2024-01-15 16:17:39 -0500

    add some new auto generated queries

--- spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/repository/UserRepository.java
+++ spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/repository/UserRepository.java
@@ -4,6 +4,12 @@ import com.pjlsoftware.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<List<User>> findByEnabledIsTrue();
+
+    Optional<User> findByUsername(String username);
 }
