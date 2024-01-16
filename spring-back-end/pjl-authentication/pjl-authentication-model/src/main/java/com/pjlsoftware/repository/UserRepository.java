05d8341b3765dd8faeef17858a0ddc222be730db
pjl <p.linnehan@gmail.com>
2024-01-16 07:06:06 -0500

    add additional valid queries as examples

--- spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/repository/UserRepository.java
+++ spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.pjlsoftware.repository;
 
 import com.pjlsoftware.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,12 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<List<User>> findByEnabledIsTrue();
 
+    /*
+     * All valid query methods that do the same thing:
+     * */
     Optional<User> findByUsername(String username);
+    //    @Query(value = "SELECT user FROM User user where user.username = :username")
+    //    Optional<User> findByUsername(String username);
+    //    @Query(nativeQuery = true, value = "SELECT * FROM users where username = :username")
+    //    Optional<User> findByUsername(String username);
 }
