defaa15c1a5fa1ab4de4d7fcdeca8c055b654f27
pjl <p.linnehan@gmail.com>
2024-01-15 21:07:21 -0500

    enable forms to we can use ngModel

--- angular-front-end/src/app/pjl-shared/shared.module.ts
+++ angular-front-end/src/app/pjl-shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   declarations: [],
-  exports: [CommonModule],
+  exports: [CommonModule, FormsModule],
 })
 export class PjlSharedModule {}
