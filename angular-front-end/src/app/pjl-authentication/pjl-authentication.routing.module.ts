6e07254df89251d122d2e5037d763d15deb2974e
pjl <p.linnehan@gmail.com>
2024-01-15 21:13:43 -0500

    wire dashboard to route

--- angular-front-end/src/app/pjl-authentication/pjl-authentication.routing.module.ts
+++ angular-front-end/src/app/pjl-authentication/pjl-authentication.routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationDashboardComponent } from './pjl-authentication-components/authentication-dashboard/authentication-dashboard.component';
 import { CreateUserButtonComponent } from './pjl-authentication-components/create-user-button/create-user-button.component';
 import { DeleteUserButtonComponent } from './pjl-authentication-components/delete-user-button/delete-user-button.component';
 import { ViewUsersComponent } from './pjl-authentication-components/view-users/view-users.component';
@@ -13,6 +14,7 @@ const routes: Routes = [
     path: 'delete-user',
     component: DeleteUserButtonComponent,
   },
+  { path: 'home', component: AuthenticationDashboardComponent },
   {
     path: 'view-users',
     component: ViewUsersComponent,
