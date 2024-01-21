64fc31d56fc51acf58155701ae80d0571e157529
pjl <p.linnehan@gmail.com>
2024-01-20 20:50:16 -0500

    refactor angular back to the original layout they suggested with
    the out of the box demo app

--- angular-front-end/src/app/pjl-shared/shared.module.ts
+++ angular-front-end/src/app/pjl-shared/shared.module.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, HttpClientModule],
+  imports: [CommonModule, FormsModule],
   declarations: [],
   exports: [CommonModule, FormsModule],
 })
--- angular-front-end/src/main.ts
+++ angular-front-end/src/main.ts
@@ -1,6 +1,7 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-platformBrowser()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.log(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
