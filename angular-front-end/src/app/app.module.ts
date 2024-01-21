07647f31ba9d2955ba32328a912d8301feb77646
pjl <p.linnehan@gmail.com>
2024-01-20 20:18:32 -0500

    add a front-end interceptor and refactor a bit to use later functions

--- angular-front-end/src/app/app.module.ts
+++ angular-front-end/src/app/app.module.ts
@@ -1,3 +1,10 @@
+import {
+  HttpHandlerFn,
+  HttpInterceptorFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
@@ -6,6 +13,20 @@ import { PjlAuthenticationModule } from './pjl-authentication/pjl-authentication
 import { PjlDemoAModule } from './pjl-demo-a/pjl-demo-a.module';
 import { PjlDemoBModule } from './pjl-demo-b/pjl-demo-b.module';
 import { PjlSharedModule } from './pjl-shared/shared.module';
+
+const httpBasicAuthInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) => {
+  const basicAuthToken = btoa('user:e3865b18-7ae9-48c1-a53a-2dce2b66e98f');
+  console.log(basicAuthToken);
+  const authReq = req.clone({
+    setHeaders: {
+      Authorization: `Basic ${basicAuthToken}`,
+    },
+  });
+  return next(authReq);
+};
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,7 +40,7 @@ import { PjlSharedModule } from './pjl-shared/shared.module';
     PjlDemoBModule,
     PjlAuthenticationModule,
   ],
-  providers: [],
+  providers: [provideHttpClient(withInterceptors([httpBasicAuthInterceptor]))],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
