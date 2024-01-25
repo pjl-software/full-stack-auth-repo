0ea2d67c005138e88a408a51118d2127c885c606
pjl <p.linnehan@gmail.com>
2024-01-24 21:51:53 -0500

    add temporary new controller to validate and work with google jwt on back-end

--- spring-back-end/pjl-authentication/pjl-authentication-core/src/main/java/com/pjlsoftware/controller/UserController.java
+++ spring-back-end/pjl-authentication/pjl-authentication-core/src/main/java/com/pjlsoftware/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.pjlsoftware.controller;
 
+import com.pjlsoftware.controller.security.ValidateGoogleAuthToken;
 import com.pjlsoftware.entity.User;
 import com.pjlsoftware.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/users")
-@CrossOrigin
 public class UserController {
 
     private final UserRepository userRepository;
@@ -23,6 +25,23 @@ public class UserController {
     @Autowired
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @RequestMapping(
+            value = "/create/google",
+            method = RequestMethod.POST,
+            produces = {"application/json"}
+    )
+    public ResponseEntity<String> createGoogleUser(@RequestParam String credential) {
+        Optional<User> user = ValidateGoogleAuthToken.verifyGoogleAuthToken(credential);
+
+        if (user.isPresent()) {
+            System.out.println(user.get().toString());
+        } else {
+            System.out.println("failed to validate");
+        }
+
+        return new ResponseEntity<>("{\"value\": \"Created new user\"}", HttpStatus.CREATED);
     }
 
     @RequestMapping(
