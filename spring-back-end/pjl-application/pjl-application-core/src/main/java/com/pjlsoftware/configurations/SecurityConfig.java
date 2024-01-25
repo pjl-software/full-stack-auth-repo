e91a3dd49e26c4f028d2d5900814035fa97f287f
pjl <p.linnehan@gmail.com>
2024-01-24 21:48:37 -0500

    add an oauth with jwt security config

--- spring-back-end/pjl-application/pjl-application-core/src/main/java/com/pjlsoftware/configurations/SecurityConfig.java
+++ spring-back-end/pjl-application/pjl-application-core/src/main/java/com/pjlsoftware/configurations/SecurityConfig.java
@@ -2,37 +2,65 @@ package com.pjlsoftware.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-
     /**
-     * Default, out-of-the-box security for all APIs. Will get a 401 unauthorized unless you attach the
-     * Authorization: Basic header in the curl request with the default username 'user' and the randomly generated
-     * password when the spring-boot starter starts (don't ever do in production). The username and password must be
-     * formatted as base64 encoded username:password. E.g.
+     * Implementing OAuth2 authentication via JSON Web Tokens (JWT). Setting up the SecurityFilterChain with:
+     * - CORS: May not be needed anymore, but was in previous versions of Spring
+     * - CSRF: Disabled to allow default disable HTTP Methods like POST to work with valid authentication
+     * - Authorization: Deny all request by default while allowing specific path patterns
+     * - The actuator health check is publicly accessible without authentication
+     * - The Custom Controller requires authentication and supports GET/POST/PUT requests only.
      * <p>
-     * curl -X GET -H "Authorization: Basic $(echo -n 'user:83b6a674-5f62-419e-8068-8bfc04231b4a' | base64)" \ https://localhost:8443/api/v1/users/
+     * Example Authorization Usage:
+     * curl -X GET -H "Authorization: Bearer {JWT}"  https://localhost:8443/api/v1/users/
      *
      * @param http
      * @return
      * @throws Exception
      */
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/actuator/**").permitAll()
-                        .anyRequest().authenticated()
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                        //
+                        .requestMatchers(HttpMethod.GET, "/api/v1/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/**").authenticated()
+                        .anyRequest().denyAll()
                 )
-                .httpBasic(withDefaults()) // Ref: https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter/
-                .csrf(AbstractHttpConfigurer::disable) // Ref: https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#servlet-csrf-configure-disable
-        ;
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
         return http.build();
+    }
+
+    /**
+     * For now, using the single decorder registration with Google since that's the only authentication I'm supporting
+     * in this branch. Maybe you can add more.
+     * <p>
+     * Ref: <a href="https://github.com/spring-projects/spring-security/issues/10943">...</a>
+     *
+     * @return - JwtDecoder with issuerLocation matching "iss" from decoded Google JWT Access Token here:
+     * <a href="https://jwt.io/">...</a>
+     */
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation("https://accounts.google.com");
     }
 }
