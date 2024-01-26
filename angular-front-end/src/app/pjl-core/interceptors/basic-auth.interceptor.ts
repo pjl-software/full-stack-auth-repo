0a1b174d5ca02aeddada247a8f8658960b97d0ab
pjl <p.linnehan@gmail.com>
2024-01-25 21:35:04 -0500

    enable custom username and password

--- angular-front-end/src/app/pjl-core/interceptors/basic-auth.interceptor.ts
+++ angular-front-end/src/app/pjl-core/interceptors/basic-auth.interceptor.ts
@@ -8,8 +8,8 @@ export const httpBasicAuthInterceptor: HttpInterceptorFn = (
   req: HttpRequest<unknown>,
   next: HttpHandlerFn
 ) => {
-  const username = 'user';
-  const password = 'bb8a2513-a7f0-4a78-9a2d-c5dae2402880';
+  const username = 'pjl';
+  const password = 'abc123';
   const encodedBasicAuthToken = btoa(username + ':' + password);
   const authReq = req.clone({
     setHeaders: {
