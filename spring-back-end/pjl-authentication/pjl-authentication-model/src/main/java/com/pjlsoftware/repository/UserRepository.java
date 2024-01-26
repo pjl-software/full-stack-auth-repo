10cef39fd15cfe32efb1aa3559bf03cdb4b880fb
pjl <p.linnehan@gmail.com>
2024-01-25 20:49:41 -0500

    remove code from old branches

--- spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/repository/UserRepository.java
+++ spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.pjlsoftware.repository;
 import com.pjlsoftware.entity.User;
 import com.pjlsoftware.projection.UserProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,14 +12,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<List<UserProjection>> findByEnabledIsTrue();
 
-    /*
-     * All valid query methods that do the same thing:
-     * */
     Optional<User> findByUsername(String username);
-
-    //    @Query(value = "SELECT user FROM User user where user.username = :username")
-    //    Optional<User> findByUsername(String username);
-
-    //    @Query(nativeQuery = true, value = "SELECT * FROM users where username = :username")
-    //    Optional<User> findByUsername(String username);
 }
