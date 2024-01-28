9c2e33ae00302e2bbd7ebc034cb5427f8f962f64
pjl <p.linnehan@gmail.com>
2024-01-27 21:55:55 -0500

    add controller path

--- angular-front-end/src/environment-configs/environment.local.ts
+++ angular-front-end/src/environment-configs/environment.local.ts
@@ -6,6 +6,7 @@ export const environment = {
   environment: 'local',
   backEndControllerPaths: {
     UserController: {
+      createGoogleUser: '/users/create/google',
       createUser: '/users/create',
       deleteUser: '/users/delete',
       getEnabledUsers: '/users/',
