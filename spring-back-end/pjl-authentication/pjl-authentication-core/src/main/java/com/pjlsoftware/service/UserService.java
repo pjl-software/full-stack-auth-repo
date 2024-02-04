dd8f8ec0fb40f8bb4c0200395913e26f36e436c5
pjl <p.linnehan@gmail.com>
2024-02-04 13:16:24 -0500

    refactor business logic into a service

--- spring-back-end/pjl-authentication/pjl-authentication-core/src/main/java/com/pjlsoftware/service/UserService.java
+++ spring-back-end/pjl-authentication/pjl-authentication-core/src/main/java/com/pjlsoftware/service/UserService.java
@@ -1,2 +1,17 @@
-package com.pjlsoftware.service;public interface UserSerivce {
+package com.pjlsoftware.service;
+
+import org.springframework.http.ResponseEntity;
+
+public interface UserService {
+
+    /**
+     * Validate Google JWT, create User Entity from JWT info then:
+     * - Log in existing user
+     * - Re-enable disabled user adding back role(s)
+     * - Create new user with role(s)
+     *
+     * @param jwt - Google JWT
+     * @return - Response entity with JSON string indicating status.
+     */
+    ResponseEntity<String> handleGoogleSignIn(final String jwt);
 }
--- spring-back-end/pjl-authentication/pjl-authentication-core/src/main/java/com/pjlsoftware/service/UserServiceImpl.java
+++ spring-back-end/pjl-authentication/pjl-authentication-core/src/main/java/com/pjlsoftware/service/UserServiceImpl.java
@@ -1,2 +1,71 @@
-package com.pjlsoftware.service;public class UserServiceImpl {
+package com.pjlsoftware.service;
+
+import com.pjlsoftware.constants.RoleName;
+import com.pjlsoftware.entity.Role;
+import com.pjlsoftware.entity.User;
+import com.pjlsoftware.repository.RoleRepository;
+import com.pjlsoftware.repository.UserRepository;
+import com.pjlsoftware.security.ValidateGoogleAuthToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    @Transactional
+    public ResponseEntity<String> handleGoogleSignIn(final String jwt) {
+        try {
+            User user = ValidateGoogleAuthToken.verifyGoogleAuthToken(jwt)
+                    .orElseThrow(() -> new RuntimeException("Failed to validate JWT."));
+
+            Role freeUserRole = roleRepository.findByName(RoleName.ROLE_FREE_USER)
+                    .orElseThrow(() -> new RuntimeException("Couldn't find the " + RoleName.ROLE_FREE_USER +
+                            " role. Are you sure you loaded the database?"));
+
+            Optional<User> isAlreadyUser = userRepository.findByUsername(user.getUsername());
+
+            if (isAlreadyUser.isPresent()) {
+                User existingUser = isAlreadyUser.get();
+
+                if (existingUser.isEnabled()) {
+                    return new ResponseEntity<>("{\"value\": \"Logging in active user\"}", HttpStatus.CREATED);
+                } else {
+                    existingUser.setEnabled(true);
+                    existingUser.setRoles(new HashSet<>(Set.of(freeUserRole)));
+
+                    userRepository.saveAndFlush(existingUser);
+                    return new ResponseEntity<>("{\"value\": \"Re-enabled existing user\"}", HttpStatus.CREATED);
+                }
+            } else {
+                user.setRoles(new HashSet<>(Set.of(freeUserRole)));
+
+                userRepository.saveAndFlush(user);
+                return new ResponseEntity<>("{\"value\": \"Created new user\"}", HttpStatus.CREATED);
+            }
+        } catch (Exception e) {
+            LOGGER.info("Exception in handleGoogleSignIn", e);
+            return new ResponseEntity<>("{\"value\": \"Google sign-in failed.\"}", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
