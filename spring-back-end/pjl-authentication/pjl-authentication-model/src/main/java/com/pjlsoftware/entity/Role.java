69f449ce5d107fa4c1b4a76e83d7e73f2d0d02e3
pjl <p.linnehan@gmail.com>
2024-02-04 13:16:40 -0500

    create new role entity, table, and repo for managing roles

--- spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/entity/Role.java
+++ spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/entity/Role.java
@@ -1,2 +1,52 @@
-package com.pjlsoftware.entity;public class Role {
+package com.pjlsoftware.entity;
+
+import com.pjlsoftware.constants.RoleName;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60)
+    private RoleName name;
+
+    public Role() {
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public RoleName getName() {
+        return name;
+    }
+
+    public void setName(RoleName name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "roleId=" + roleId +
+                ", name=" + name +
+                '}';
+    }
 }
--- spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/repository/RoleRepository.java
+++ spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/repository/RoleRepository.java
@@ -1,2 +1,13 @@
-package com.pjlsoftware.repository;public interface RoleRepository {
+package com.pjlsoftware.repository;
+
+import com.pjlsoftware.constants.RoleName;
+import com.pjlsoftware.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
 }
