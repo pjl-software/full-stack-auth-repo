896fccb370df5e6d20d4568c613741194262c75e
pjl <p.linnehan@gmail.com>
2024-02-05 22:14:32 -0500

    include roles and embedded projections

--- spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/projection/UserProjection.java
+++ spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/projection/UserProjection.java
@@ -1,9 +1,15 @@
 package com.pjlsoftware.projection;
 
+import com.pjlsoftware.entity.Role;
+
+import java.util.Set;
+
 public interface UserProjection {
     String getFirstName();
 
     String getLastName();
 
     String getUsername();
+
+    Set<RoleProjection> getRoles();
 }
