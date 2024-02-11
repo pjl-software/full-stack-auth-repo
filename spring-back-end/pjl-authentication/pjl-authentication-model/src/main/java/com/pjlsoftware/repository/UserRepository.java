341e9617d919606d2e400a499f55ecd4f03b719c
pjl <p.linnehan@gmail.com>
2024-02-11 13:22:33 -0500

    add new queries

--- spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/repository/UserRepository.java
+++ spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.pjlsoftware.repository;
 
 import com.pjlsoftware.entity.User;
-import com.pjlsoftware.projection.UserProjection;
+import com.pjlsoftware.projection.AuthenticatedUserProjection;
+import com.pjlsoftware.projection.GenericUserProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,9 +12,12 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<List<UserProjection>> findByEnabledIsTrue();
+    Optional<List<GenericUserProjection>> findByEnabledIsTrue();
 
     Optional<User> findByUsername(String username);
+
+    @Query(value = "SELECT user from User user where user.username = :username and user.enabled = true ")
+    Optional<AuthenticatedUserProjection> returnUserInfoForEnabledUserByUsername(String username);
 
     Optional<User> findByUsernameAndEnabledIsTrue(String username);
 }
