c06912a90e69773ee01806c35492b32ce0afb89e
pjl <p.linnehan@gmail.com>
2024-02-11 13:21:50 -0500

    rearrange and refactor the user service
    return a user when creating a new google user

--- spring-back-end/pjl-authentication/pjl-authentication-core/src/main/java/com/pjlsoftware/service/UserService.java
+++ spring-back-end/pjl-authentication/pjl-authentication-core/src/main/java/com/pjlsoftware/service/UserService.java
@@ -1,10 +1,26 @@
 package com.pjlsoftware.service;
 
 import com.pjlsoftware.entity.User;
+import com.pjlsoftware.projection.AuthenticatedUserProjection;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
+import java.util.Optional;
+
 public interface UserService {
+
+    /**
+     * @param principal - Value from @AuthenticationPrincipal
+     * @return - A valid user from our database associated with the principal; otherwise null
+     */
+    User getUserFromJwt(JwtAuthenticationToken principal);
+
+    /**
+     *
+     * @param username
+     * @return
+     */
+    Optional<AuthenticatedUserProjection> getUserInformation(String username);
 
     /**
      * Validate Google JWT, create User Entity from JWT info then:
@@ -15,11 +31,5 @@ public interface UserService {
      * @param jwt - Google JWT
      * @return - Response entity with JSON string indicating status.
      */
-    ResponseEntity<String> handleGoogleSignIn(final String jwt);
-
-    /**
-     * @param principal - Value from @AuthenticationPrincipal
-     * @return - A valid user from our database associated with the principal; otherwise null
-     */
-    User getUserFromJwt(JwtAuthenticationToken principal);
+    ResponseEntity<AuthenticatedUserProjection> handleGoogleSignIn(final String jwt);
 }
--- spring-back-end/pjl-authentication/pjl-authentication-core/src/main/java/com/pjlsoftware/service/UserServiceImpl.java
+++ spring-back-end/pjl-authentication/pjl-authentication-core/src/main/java/com/pjlsoftware/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.pjlsoftware.service;
 import com.pjlsoftware.authenticationConstants.RoleName;
 import com.pjlsoftware.entity.Role;
 import com.pjlsoftware.entity.User;
+import com.pjlsoftware.projection.AuthenticatedUserProjection;
 import com.pjlsoftware.repository.RoleRepository;
 import com.pjlsoftware.repository.UserRepository;
 import com.pjlsoftware.security.ValidateGoogleAuthToken;
@@ -35,47 +36,6 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    @Transactional
-    public ResponseEntity<String> handleGoogleSignIn(final String jwt) {
-        try {
-            User user = ValidateGoogleAuthToken.verifyGoogleAuthToken(jwt)
-                    .orElseThrow(() -> new RuntimeException("Failed to validate JWT."));
-
-            Role freeUserRole = roleRepository.findByName(RoleName.ROLE_FREE_USER)
-                    .orElseThrow(() -> new RuntimeException("Couldn't find the " + RoleName.ROLE_FREE_USER +
-                            " role. Are you sure you loaded the database?"));
-            Role adminUserRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
-                    .orElseThrow(() -> new RuntimeException("Couldn't find the " + RoleName.ROLE_ADMIN +
-                            " role. Are you sure you loaded the database?"));
-            Set<Role> googleUserRoles = new HashSet<>(Set.of(freeUserRole, adminUserRole));
-
-            Optional<User> isAlreadyUser = userRepository.findByUsername(user.getUsername());
-
-            if (isAlreadyUser.isPresent()) {
-                User existingUser = isAlreadyUser.get();
-
-                if (existingUser.isEnabled()) {
-                    return new ResponseEntity<>("{\"value\": \"Logging in active user\"}", HttpStatus.CREATED);
-                } else {
-                    existingUser.setEnabled(true);
-                    existingUser.setRoles(googleUserRoles);
-
-                    userRepository.saveAndFlush(existingUser);
-                    return new ResponseEntity<>("{\"value\": \"Re-enabled existing user\"}", HttpStatus.CREATED);
-                }
-            } else {
-                user.setRoles(googleUserRoles);
-
-                userRepository.saveAndFlush(user);
-                return new ResponseEntity<>("{\"value\": \"Created new user\"}", HttpStatus.CREATED);
-            }
-        } catch (Exception e) {
-            LOGGER.info("Exception in handleGoogleSignIn", e);
-            return new ResponseEntity<>("{\"value\": \"Google sign-in failed.\"}", HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @Override
     public User getUserFromJwt(JwtAuthenticationToken principal) {
         try {
             User user = ValidateGoogleAuthToken.verifyGoogleAuthToken(principal.getToken().getTokenValue())
@@ -85,6 +45,59 @@ public class UserServiceImpl implements UserService {
         } catch (Exception e) {
             LOGGER.info("Exception in getUserFromJwt", e);
         }
+        return new User();
+    }
+
+    @Override
+    public Optional<AuthenticatedUserProjection> getUserInformation(String username) {
+        try {
+            return userRepository.returnUserInfoForEnabledUserByUsername(username);
+        } catch (Exception e) {
+
+        }
         return null;
+    }
+
+    @Override
+    @Transactional
+    public ResponseEntity<AuthenticatedUserProjection> handleGoogleSignIn(final String jwt) {
+        try {
+            LOGGER.info("Made it here...");
+            User user = ValidateGoogleAuthToken.verifyGoogleAuthToken(jwt)
+                    .orElseThrow(() -> new RuntimeException("Failed to validate JWT."));
+
+            Role freeUserRole = roleRepository.findByName(RoleName.ROLE_FREE_USER)
+                    .orElseThrow(() -> new RuntimeException("Couldn't find the " + RoleName.ROLE_FREE_USER +
+                            " role. Are you sure you loaded the database?"));
+            Role adminUserRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
+                    .orElseThrow(() -> new RuntimeException("Couldn't find the " + RoleName.ROLE_ADMIN +
+                            " role. Are you sure you loaded the database?"));
+            Set<Role> googleUserRoles = new HashSet<>(Set.of(freeUserRole));
+
+            Optional<User> isAlreadyUser = userRepository.findByUsername(user.getUsername());
+
+            if (isAlreadyUser.isPresent()) {
+                User existingUser = isAlreadyUser.get();
+
+                if (existingUser.isEnabled()) {
+
+                } else {
+                    existingUser.setEnabled(true);
+                    existingUser.setRoles(googleUserRoles);
+                    userRepository.saveAndFlush(existingUser);
+
+                }
+            } else {
+                user.setRoles(googleUserRoles);
+                userRepository.saveAndFlush(user);
+            }
+
+            return new ResponseEntity<>(userRepository.returnUserInfoForEnabledUserByUsername(user.getUsername())
+                    .orElseThrow(() -> new RuntimeException("Failed to get username"))
+                    , HttpStatus.CREATED);
+        } catch (Exception e) {
+            LOGGER.info("Exception in handleGoogleSignIn", e);
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
 }
