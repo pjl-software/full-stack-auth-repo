df78440614099ad811175e8b9d7605519027870c
pjl <p.linnehan@gmail.com>
2024-03-02 13:02:56 -0500

    update prod env variables

--- angular-front-end/src/environment-configs/environment.prod.ts
+++ angular-front-end/src/environment-configs/environment.prod.ts
@@ -1,6 +1,19 @@
 export const environment = {
   production: true,
   //
-  apiUrl: '',
+  apiVersion: '/v1',
+  apiUrl: 'https://api.demo.pjlindustries.com',
+  appUrl: 'https://demo.pjlindustries.com',
   environment: 'prod',
+  backEndControllerPaths: {
+    AuthController: {
+      createGoogleUser: '/auth/sign-in/google',
+    },
+    UserController: {
+      createUser: '/users/create',
+      deleteUser: '/users/delete',
+      getEnabledUsers: '/users/',
+      getUserInformation: '/users/info',
+    },
+  },
 };
