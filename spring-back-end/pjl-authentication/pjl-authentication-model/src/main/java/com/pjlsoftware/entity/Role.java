778feda62221259bd18506b21cfd8f780e6cc43a
pjl <p.linnehan@gmail.com>
2024-03-02 20:01:18 -0500

    extend date audit

--- spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/entity/Role.java
+++ spring-back-end/pjl-authentication/pjl-authentication-model/src/main/java/com/pjlsoftware/entity/Role.java
@@ -13,7 +13,7 @@ import org.hibernate.annotations.NaturalId;
 
 @Entity
 @Table(name = "roles")
-public class Role {
+public class Role extends DateAudit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long roleId;
